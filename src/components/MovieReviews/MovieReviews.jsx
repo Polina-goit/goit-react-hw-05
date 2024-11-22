5c2656de6c96c6cb69cd8a31f97fda3d0df88072
Polina <polinakoctyu4enko@gmail.com>
2024-11-22 19:51:28 +0200

    step -10

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchMovieReview } from "../../assets/api";
+import { useParams } from "react-router-dom";
 
 const MovieReviews = () => {
-  return <div>Reviews</div>;
+  const { movieId } = useParams();
+  const [movieReview, setMovieReview] = useState(null);
+
+  useEffect(() => {
+    const fetchReviwes = async () => {
+      try {
+        const reviewes = await fetchMovieReview(movieId);
+        setMovieReview(reviewes);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchReviwes();
+  }, [movieId]);
+
+  return (
+    <div>
+      {!movieReview || movieReview.results.length === 0 ? (
+        <p>Reviews is not found</p>
+      ) : (
+        <ul>
+          {movieReview.results.map(({ id, author, content }) => (
+            <li key={id}>
+              <h3>Author: {author}</h3>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MovieReviews;
